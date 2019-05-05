e42bba5892654892809426d89c660a5a12d7175f
Volodymyr Shcherbyna <volodymyr@shcherbyna.com>
2019-05-05 23:05:43 +0200

    release version 1.5.0

--- src/main.cpp
+++ src/main.cpp
@@ -47,7 +47,7 @@
 #endif
 
 const string PROGRAM_NAME   = "Igel";
-const string VERSION        = "1.4.3.4";
+const string VERSION        = "1.5.0";
 
 const int MIN_HASH_SIZE     = 1;
 const int MAX_HASH_SIZE     = 131072;
