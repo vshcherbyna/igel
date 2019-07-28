3cc3e64e44bb2a4cc984de21c36067c9af7e74df
Volodymyr Shcherbyna <volodymyr@shcherbyna.com>
2019-07-28 13:55:42 +0200

    drop into qsearch at depth <= 0 without checking for tt (#91)
    see pruning
    fix root node detection
    avoid null move prunning if a tt hit discovered
    tc=all/10+0.1
    Score of Igel 1.8.3 64 POPCNT vs Igel 1.8.2 64 POPCNT: 3145 - 1879 - 3220  [0.577] 8244
    Elo difference: 53.78 +/- 5.87
    tc=40/96
    Score of Igel 1.8.3 64 POPCNT vs Igel 1.8.2 64 POPCNT: 170 - 103 - 320 [0.556] 593
    Elo difference: 39.42 +/- 18.94
    tc=40/96
    Rank Name                          Elo     +/-   Games   Score   Draws
       0 Igel 1.8.3 64 POPCNT           16      19     900   52.3%   29.7%
       1 zurichess neuchatel           100      60     100   64.0%   28.0%
       2 RuyDos 1.1.10                  42      59     100   56.0%	 26.0%
       3 gogobello 2.0                  38      56     100   55.5%   33.0%
       4 Monolith 1.0                   38      55     100   55.5%   37.0%
       5 Shield 2.1                    -28      58     100   46.0%   28.0%
       6 Cheese 2.1 64 bits            -63      61     100   41.0%   24.0%
       7 XboardEngine                  -67      58     100   40.5%   31.0%
       8 Arminius 2018-12-23           -85      58	   100   38.0%   32.0%
       9 Fridolin 3.10                -123      61     100   33.0%   28.0%

--- src/uci.cpp
+++ src/uci.cpp
@@ -26,7 +26,7 @@
 #include <iostream>
 #include <sstream>
 
-const std::string VERSION = "1.8.2";
+const std::string VERSION = "1.8.3";
 
 const int MIN_HASH_SIZE = 1;
 const int MAX_HASH_SIZE = 131072;
