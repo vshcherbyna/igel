ed55e50a405310f9b07d8c9dcf22d035d1dc43c4
Volodymyr Shcherbyna <volodymyr@shcherbyna.com>
2019-09-24 20:23:17 +0200

    tunning (#99)
    * tunning
    Score of Igel 1.9.2 64 POPCNT vs Igel 1.9.0 64 POPCNT: 60 - 24 - 101 [0.597] 185
    Elo difference: 68.48 +/- 33.64
    
    * tunning of evaluation parameters
    tc=all/10+0.1
    Rank Name                          Elo     +/-   Games   Score   Draws
    0 Igel 1.9.2 64 POPCNT         -296       8   10000   15.4%   16.3%
    1 Stockfish 10 64 POPCNT        739      44    2500   98.6%   2.5%
    2 Ethereal 11.50 (POPCNT)       513      24    2500   95.0%   8.2%
    3 RubiChess 1.5                 259      14    2500   81.6%   22.8%
    4 Cheng 4.39                     93      11    2500   63.1%   31.8%
    tc=40/102
    Score of Igel 1.9.2 64 POPCNT vs Winter 0.7 BMI2: 56 - 76 - 72  [0.451] 204
    Elo difference: -34.17 +/- 38.54

--- src/uci.cpp
+++ src/uci.cpp
@@ -28,7 +28,7 @@
 #include <iostream>
 #include <sstream>
 
-const std::string VERSION = "1.9.1";
+const std::string VERSION = "1.9.2";
 
 const int MIN_HASH_SIZE = 1;
 const int MAX_HASH_SIZE = 131072;
