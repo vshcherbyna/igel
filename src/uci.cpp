04ccc69a01c41354ae2ccf7c757740fc18c61d31
Volodymyr Shcherbyna <volodymyr@shcherbyna.com>
2020-01-01 01:58:22 +0100

    Release Igel 2.3.0 (TCEC S17 QL version) os=linux hash=256 tc=40/102 (CCRL 40/4) Score of Igel 2.3.0 64 POPCNT vs Igel 2.2.2 64 POPCNT: 331 - 195 - 805  [0.551] 1331 Elo difference: 35.62 +/- 11.69

--- src/uci.cpp
+++ src/uci.cpp
@@ -28,7 +28,7 @@
 #include <iostream>
 #include <sstream>
 
-const std::string VERSION = "2.2.2 +1";
+const std::string VERSION = "2.3.0";
 
 const int MIN_HASH_SIZE = 1;
 const int MAX_HASH_SIZE = 1048576;
