ddcc3ef5c44d6503026f1618cf5b1822e525b743
Volodymyr Shcherbyna <volodymyr@shcherbyna.com>
2021-04-17 18:34:51 +0200

    3.1-dev-2: skip null move pruning in tt hit

--- src/search.cpp
+++ src/search.cpp
@@ -164,9 +164,10 @@ EVAL Search::abSearch(EVAL alpha, EVAL beta, int depth, int ply, bool isNull, bo
 
     Move hashMove{};
     TEntry hEntry{};
-    EVAL ttScore;
-    auto onPV  = (beta - alpha > 1);
-    auto ttHit = !skipMove && !isNull && ProbeHash(hEntry);
+
+    EVAL ttScore = 0;
+    auto onPV    = (beta - alpha > 1);
+    auto ttHit   = !skipMove && !isNull && ProbeHash(hEntry);
 
     if (ttHit) {
         ttScore = hEntry.m_data.score;
@@ -286,7 +287,7 @@ EVAL Search::abSearch(EVAL alpha, EVAL beta, int depth, int ply, bool isNull, bo
         //   null move
         //
 
-        if (!isNull && depth >= 2 && m_position.NonPawnMaterial() && bestScore >= beta) {
+        if (!isNull && depth >= 2 && bestScore >= beta && (!ttHit || !(hEntry.m_data.type == HASH_BETA) || ttScore >= beta) && m_position.NonPawnMaterial()) {
             int R = 5 + depth / 6 + std::min(3, (bestScore - beta) / 100);
 
             m_position.MakeNullMove();
