297b8afec1d343c518f146968f36c812fdab9509
Volodymyr Shcherbyna <volodymyr@shcherbyna.com>
2021-07-09 20:42:06 +0200

    3.0.8: razoring 150 cp

--- src/search.cpp
+++ src/search.cpp
@@ -276,7 +276,7 @@ EVAL Search::abSearch(EVAL alpha, EVAL beta, int depth, int ply, bool isNull, bo
         //   razoring
         //
 
-        if (depth <= 1 && staticEval + 325 < alpha)
+        if (depth <= 1 && staticEval + 150 < alpha)
             return qSearch(alpha, beta, ply, 0);
 
         //
