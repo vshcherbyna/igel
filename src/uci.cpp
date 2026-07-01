af793b525dceb343fd2cebaa8d9099bde568cd12
Volodymyr Shcherbyna <volodymyr@shcherbyna.com>
2026-07-02 00:15:52 +0100

    3.6.13: fix history overflow

--- src/uci.cpp
+++ src/uci.cpp
@@ -31,7 +31,7 @@
 #include <iostream>
 #include <sstream>
 
-const std::string VERSION = "3.6.12";
+const std::string VERSION = "3.6.13";
 #if defined(PURE_HCE)
 const std::string PROGRAM_NAME = "Igel HCE";
 #else
