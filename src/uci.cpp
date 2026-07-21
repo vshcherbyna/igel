a2d0c5b36377d35e1afa961adc7ae6ef98c5d3c3
Volodymyr Shcherbyna <volodymyr@shcherbyna.com>
2026-07-22 00:35:53 +0100

    3.6.24: optimize quiets pickup

--- src/uci.cpp
+++ src/uci.cpp
@@ -31,7 +31,7 @@
 #include <iostream>
 #include <sstream>
 
-const std::string VERSION = "3.6.23";
+const std::string VERSION = "3.6.24";
 #if defined(PURE_HCE)
 const std::string PROGRAM_NAME = "Igel HCE";
 #else
