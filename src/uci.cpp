8efbdc153c7f54f77f39bd919b610cbab43a2c98
Volodymyr Shcherbyna <volodymyr@shcherbyna.com>
2026-08-02 21:39:44 +0100

    3.6.27: detect bmi2 in basic makefile (#340)
    Elo   | 2.26 +- 1.90 (95%)
    SPRT  | 2.5+0.03s Threads=1 Hash=8MB
    LLR   | 2.97 (-2.94, 2.94) [-1.00, 1.00]
    Games | N: 49082 W: 13920 L: 13601 D: 21561
    Penta | [867, 5689, 11191, 5846, 948]
    https://chess.grantnet.us/test/40727/
    
    bench: 3905167

--- src/uci.cpp
+++ src/uci.cpp
@@ -31,7 +31,7 @@
 #include <iostream>
 #include <sstream>
 
-const std::string VERSION = "3.6.26";
+const std::string VERSION = "3.6.27";
 #if defined(PURE_HCE)
 const std::string PROGRAM_NAME = "Igel HCE";
 #else
